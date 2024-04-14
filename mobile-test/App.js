fb109c0af98cd63c38075a2d57ac8e636041ce5c
oussema_hichri <oussema_hichri@ext.carrefour.com>
2024-04-14 20:20:25 +0100

    install and setup react query

--- mobile-test/App.js
+++ mobile-test/App.js
@@ -2,20 +2,24 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import Home from './pages/Home';
 import Details from './pages/Details';
 
 
 const Stack = createNativeStackNavigator();
+const queryClient = new QueryClient();
 
 export default function App() {
   return (
+    <QueryClientProvider client={queryClient}>
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={Home}/>
         <Stack.Screen name="Details" component={Details}/>
       </Stack.Navigator>
     </NavigationContainer>
+    </QueryClientProvider>
   );
 }
 
